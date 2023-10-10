204c500ca133664a7ff783379754b25f67151c61
Sai Krishna chillapalli <112238804+Chillapallisaikrishna@users.noreply.github.com>
2023-10-10 13:04:02 +0530

    Add files via upload

---
+++ DAY_1/add.java
@@ -0,0 +1,11 @@
+class add{
+public static int add(int a,int b) {
+return a+b;
+}
+public static void main(String [] args){
+int a=10;
+int b=10;
+int sum=add(a, b);
+System.out.println("The sum of a & b is: " + sum);
+}
+}
---
+++ DAY_1/name.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class name{
+public static void main (String [] args){
+Scanner scanner= new Scanner(System.in);
+System.out.println("Enter a Name: ");
+String string = scanner.nextLine();
+System.out.println("Name is "+string);
+}
+}
